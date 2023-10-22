0afb90c5fa0e65f672879707d4a12a1ae7555594
liudebo <564901612@qq.com>
2023-10-22 19:54:03 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -8,6 +8,8 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
